c648756050be4a36ad5a777ff887dad68d6c3655
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-05-10 12:56:59 +0900

    02_00

--- KamataEngine/Novice/main.cpp
+++ KamataEngine/Novice/main.cpp
@@ -14,12 +14,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	Segment segment{
+	    {-2.0f, -1.0f, 0.0f},
+        {3.0f,  2.0f,  2.0f}
+    };
+	Vector3 point{-1.5f, 0.6f, 0.6f};
+
 	Vector3 cameraTranslate{0.0f, 1.9f, -6.49f};
 	Vector3 cameraRotate{0.26f, 0.0f, 0.0f};
 	Vector3 cameraPosition{0.0f, 1.0f, -5.0f};
-	Sphere sphere;
+	/*Sphere sphere;
 	sphere.center = {0.0f, 0.0f, 0.0f};
-	sphere.radius = 1.0f;
+	sphere.radius = 1.0f;*/
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -29,6 +35,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Novice::GetHitKeyStateAll(keys);
 		///
 		/// ↓更新処理ここから
+		
+		Vector3 project = Project(Subtract(point, segment.origin), segment.diff);
+		Vector3 closestPoint = ClosestPoint(point, segment);
+
 		Matrix4x4 CameraMatrix = MakeAffineMatrix({1.0f, 1.0f, 1.0f}, cameraRotate, Add(cameraPosition, cameraTranslate));
 		Matrix4x4 ViewMatrix = Inverse(CameraMatrix);
 		Matrix4x4 ProjectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
@@ -42,15 +52,27 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-	
+		
+		// ImGui
 		ImGui::Begin("Window");
 		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
 		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
-		ImGui::DragFloat3("SphereCenter", &sphere.center.x, 0.01f);
-		ImGui::DragFloat("SphereRadius", &sphere.radius, 0.01f);
+		ImGui::InputFloat3("Project", &project.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
+		/*ImGui::DragFloat3("SphereCenter", &sphere.center.x, 0.01f);
+		ImGui::DragFloat("SphereRadius", &sphere.radius, 0.01f);*/
 		ImGui::End();
+
+		Sphere pointSphere{point, 0.01f};// 1cmの4球を描画
+		Sphere closestPointSphere{closestPoint, 0.01f};
+		// 線分の両端をスクリーン座標系まで変換
+		Vector3 start = Transform(Transform(segment.origin, ViewProjectionMatrix), ViewportMatrix);
+		Vector3 end = Transform(Transform(Add(segment.origin, segment.diff), ViewProjectionMatrix), ViewportMatrix);
+		// 線を描画
+		Novice::DrawLine((int)start.x, (int)start.y, (int)end.x, (int)end.y, WHITE);
+
 		DrawGrid(ViewProjectionMatrix, ViewportMatrix);
-		DrawSphere(sphere, ViewProjectionMatrix, ViewportMatrix, BLACK);
+		DrawSphere(pointSphere, ViewProjectionMatrix, ViewportMatrix, RED);
+		DrawSphere(closestPointSphere, ViewProjectionMatrix, ViewportMatrix, BLACK);
 
 
 		///
